040a8b65bae1c50d6f2123a02d473d5bfebd3db9
abebed19 <abebed19@gmail.com>
2026-03-16 10:19:23 +0300

    Main method implementation

---
+++ src/EcoPointsRecyclingTracker.java
@@ -0,0 +1,21 @@
+public class EcoPointsRecyclingTracker {
+
+    public static  void main(String ... args){
+
+
+        boolean running = true;
+        while(true){
+
+            System.out.println("\n=== Eco-Points ReCycling  Tracker ===");
+            System.out.println("1. Register Household");
+            System.out.println("2. Log Recycling Event");
+            System.out.println("3. Display Households");
+            System.out.println("4. Display Household recycling Events");
+            System.out.println("5. Generate Reports");
+            System.out.println("6. Save and exit");
+            System.out.print("Choose an option:");
+
+        }
+
+    }
+}
